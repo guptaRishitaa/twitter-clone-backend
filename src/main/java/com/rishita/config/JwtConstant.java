a309ff5d6f8e4db04d8e67b1aadf0f3d48f07c90
rishita <rishitagupta2700@gmail.com>
2024-03-14 18:35:16 -0400

    Jwt Token Security

---
+++ src/main/java/com/rishita/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.rishita.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="bjhdbjkbdjbduhdwoihdbjbfoawugevqhjve";
+    public static final String JWT_HEADER="Authorization";
+}
